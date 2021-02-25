e5665f021ec2e16455188f4b7bffad7c612f4f12
Miguel Rios <miguelriosoliveira@gmail.com>
2021-02-25 09:26:19 -0300

    Day 4.1 - Finished Deposit action and added Withdraw action

--- lib/rocketpay_web/controllers/accounts_controller.ex
+++ lib/rocketpay_web/controllers/accounts_controller.ex
@@ -5,8 +5,16 @@ defmodule RocketpayWeb.AccountsController do
 
   action_fallback RocketpayWeb.FallbackController
 
-  def create(conn, params) do
+  def deposit(conn, params) do
     with {:ok, %Account{} = account} <- Rocketpay.deposit(params) do
+      conn
+      |> put_status(:ok)
+      |> render("update.json", account: account)
+    end
+  end
+
+  def withdraw(conn, params) do
+    with {:ok, %Account{} = account} <- Rocketpay.withdraw(params) do
       conn
       |> put_status(:ok)
       |> render("update.json", account: account)
